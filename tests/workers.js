4c52cff60bc9554725bfb05fd083779a273d0224
David Powell <pwnzerdragoon@gmail.com>
2011-10-25 19:33:48 -0700

    Finished web worker test

--- tests/workers.js
+++ tests/workers.js
@@ -1,1 +1,12 @@
-alert("Test");
+onmessage = function(e) {
+    if (e.data == "start") {
+        setInterval(incCounter, 1000);
+    }
+}
+
+var count = 0;
+
+function incCounter() {
+    count++;
+    postMessage(count);
+}
